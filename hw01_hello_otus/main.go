d3971fa49cad2de22136e2a0d6780b7875820667
Vladimir Novopashin <novopashinwm@mail.ru>
2022-04-28 14:27:40 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	var whatYouSay = "Hello, OTUS!"
+	fmt.Println(stringutil.Reverse(whatYouSay))
 }
