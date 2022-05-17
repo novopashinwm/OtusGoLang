7cd724dcd77b1bb4d4ea23ba43170e90bcfa817a
Vladimir Novopashin <novopashinwm@mail.ru>
2022-05-17 18:07:09 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,53 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(inStr string) (string, error) {
 	// Place your code here.
-	return "", nil
+	var sb strings.Builder
+	chars := []rune(inStr)
+	var err error
+	if len(chars) == 0 {
+		return "", nil
+	}
+	if unicode.IsDigit(chars[0]) {
+		return "", ErrInvalidString
+	}
+	var num = len(chars)
+	var ekranSymbol = false
+	for i := 0; i < num; i++ {
+		curr := string(chars[i])
+		if chars[i] == 92 && !ekranSymbol {
+			ekranSymbol = true
+		} else if i < (num-1) && unicode.IsDigit(chars[i+1]) {
+			if IsNextDigital(i, num, chars) {
+				return "", ErrInvalidString
+			}
+
+			next := string(chars[i+1])
+			var repeatNum, _ = strconv.Atoi(next)
+			if repeatNum != 0 {
+				sb.WriteString(strings.Repeat(curr, repeatNum))
+			}
+
+			i++
+		} else {
+			sb.WriteString(curr)
+			ekranSymbol = false
+		}
+	}
+	return sb.String(), err
+}
+
+func IsNextDigital(i int, num int, chars []rune) bool {
+	if i < num-2 {
+		return unicode.IsDigit(chars[i+2])
+	}
+	return false
 }
