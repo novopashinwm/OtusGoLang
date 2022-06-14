3514631872c1458334a18a1a9efc7e38dac47e32
Vladimir Novopashin <novopashinwm@mail.ru>
2022-06-14 21:11:04 +0300

    HW04 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,11 +11,54 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mutex    *sync.Mutex
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
+	ci := &cacheItem{key: key, value: value}
+	if item, ok := cache.items[key]; ok {
+		item.Value = ci
+		cache.queue.MoveToFront(item)
+		return true
+	}
+
+	if cache.queue.Len() == cache.capacity {
+		lastItem, ok := cache.queue.Back().Value.(*cacheItem)
+		if !ok {
+			return false
+		}
+		cache.queue.Remove(cache.queue.Back())
+		delete(cache.items, lastItem.key)
+	}
+	pushFront := cache.queue.PushFront(ci)
+	cache.items[key] = pushFront
+	return false
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
+	if item, ok := cache.items[key]; ok {
+		cache.queue.MoveToFront(item)
+		ci, ok := item.Value.(*cacheItem)
+		if !ok {
+			return nil, false
+		}
+		return ci.value, true
+	}
+	return nil, false
+}
+
+func (cache *lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items = make(map[Key]*ListItem, cache.capacity)
 }
 
 type cacheItem struct {
@@ -26,5 +71,6 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		mutex:    new(sync.Mutex),
 	}
 }
--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/novopashinwm/OtusGoLang/hw04_lru_cache
 
 go 1.16
 
