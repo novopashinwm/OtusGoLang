5a3de113d6e64440df7cc94b6f394a7a50126d51
Vladimir Novopashin <novopashinwm@mail.ru>
2022-07-17 22:15:22 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"path"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,42 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envMap := make(Environment)
+	filesAndDirs, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range filesAndDirs {
+		if entry.IsDir() {
+			continue
+		}
+		itemValue, err := ParseFile(path.Join(dir, entry.Name()))
+		if err != nil {
+			return nil, err
+		}
+		envMap[entry.Name()] = itemValue
+	}
+
+	return envMap, nil
+}
+
+func ParseFile(file string) (EnvValue, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return EnvValue{Value: "", NeedRemove: false}, err
+	}
+	fileStat, err := f.Stat()
+	if err != nil {
+		return EnvValue{Value: "", NeedRemove: false}, err
+	}
+	if fileStat.Size() == 0 {
+		return EnvValue{Value: "", NeedRemove: true}, nil
+	}
+	reader := bufio.NewReader(f)
+	line, _ := reader.ReadBytes('\n')
+	line = bytes.ReplaceAll(line, []byte{0}, []byte{'\n'})
+	line = bytes.TrimRight(line, " \n\t\r")
+	return EnvValue{Value: string(line), NeedRemove: false}, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for name, val := range env {
+		if val.NeedRemove {
+			os.Unsetenv(name)
+			continue
+		}
+		os.Setenv(name, val.Value)
+	}
+	cmdItem := exec.Command(cmd[0], cmd[1:]...)
+	cmdItem.Stdout = os.Stdout
+	cmdItem.Stdin = os.Stdin
+	cmdItem.Stderr = os.Stderr
+	if err := cmdItem.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			returnCode = exitError.ExitCode()
+		}
+	}
+	return returnCode
+
 }
