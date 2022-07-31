ba6def7b5b6ca4ed69bd2427ab33278e6f62b51e
Vladimir Novopashin <novopashinwm@mail.ru>
2022-07-31 17:19:52 +0300

    HW9 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,289 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+var (
+	ErrUnsupportedType = errors.New("unsupported type")
+	ErrInvalidRule     = errors.New("invalid validation rule")
+
+	ErrNotInList      = errors.New("value must exist in list")
+	ErrExactLen       = errors.New("value must be exact length")
+	ErrLessOrEqual    = errors.New("value must be less or equal")
+	ErrGreaterOrEqual = errors.New("value must be greater or equal")
+	ErrMatchRegExp    = errors.New("value must match regular expression")
+)
+
+type (
+	intValidator struct {
+		min, max int64
+		in       []int64
+	}
+
+	stringValidator struct {
+		len int
+		in  []string
+		re  *regexp.Regexp
+	}
+)
+
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var builder strings.Builder
+
+	for _, err := range v {
+		builder.WriteString(fmt.Sprintf("%s: %s\n", err.Field, err.Err.Error()))
+	}
+
+	return builder.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	var validationErrs ValidationErrors
+
+	refValue := reflect.ValueOf(v)
+	if refValue.Kind() != reflect.Struct {
+		return ErrUnsupportedType
+	}
+
+	filedCount := refValue.NumField()
+	for i := 0; i < filedCount; i++ {
+		fieldValue := refValue.Field(i)
+		if !fieldValue.CanInterface() {
+			continue
+		}
+
+		fieldType := refValue.Type().Field(i)
+		tag := fieldType.Tag.Get("validate")
+		if tag == "" {
+			continue
+		}
+
+		switch fieldValue.Kind() { //nolint:exhaustive
+		case reflect.Int:
+			validator, err := parseIntFieldValidationRules(tag)
+			if err != nil {
+				return err
+			}
+
+			if err := validator.validate(fieldValue.Int()); err != nil {
+				validationErrs = append(validationErrs, ValidationError{Field: fieldType.Name, Err: err})
+			}
+		case reflect.String:
+			validator, err := parseStringFieldValidationRules(tag)
+			if err != nil {
+				return err
+			}
+
+			if err := validator.validate(fieldValue.String()); err != nil {
+				validationErrs = append(validationErrs, ValidationError{Field: fieldType.Name, Err: err})
+			}
+		case reflect.Slice:
+			switch val := fieldValue.Interface().(type) {
+			case []int:
+				validator, err := parseIntFieldValidationRules(tag)
+				if err != nil {
+					return err
+				}
+
+				if err := validator.validateSlice(fieldType.Name, val); err != nil {
+					validationErrs = append(validationErrs, err...)
+				}
+			case []string:
+				validator, err := parseStringFieldValidationRules(tag)
+				if err != nil {
+					return err
+				}
+
+				if err := validator.validateSlice(fieldType.Name, val); err != nil {
+					validationErrs = append(validationErrs, err...)
+				}
+			}
+		}
+	}
+
+	if len(validationErrs) != 0 {
+		return validationErrs
+	}
+	return nil
+}
+
+func parseStringFieldValidationRules(tag string) (stringValidator, error) {
+	var validator stringValidator
+
+	rules := strings.Split(tag, "|")
+
+	for _, rule := range rules {
+		parts := strings.Split(rule, ":")
+		if len(parts) != 2 {
+			continue
+		}
+
+		switch parts[0] {
+		case "len":
+			v, err := strconv.Atoi(parts[1])
+			if err != nil {
+				return validator, fmt.Errorf("%w: len must be a valid integer", ErrInvalidRule)
+			}
+
+			validator.len = v
+		case "regexp":
+			re, err := regexp.Compile(parts[1])
+			if err != nil {
+				return validator, fmt.Errorf("%w: regexp must contain valid regular expression", ErrInvalidRule)
+			}
+
+			validator.re = re
+		case "in":
+			validator.in = strings.Split(parts[1], ",")
+		}
+	}
+
+	return validator, nil
+}
+
+func parseIntFieldValidationRules(tag string) (intValidator, error) {
+	var validator intValidator
+
+	rules := strings.Split(tag, "|")
+
+	for _, rule := range rules {
+		parts := strings.Split(rule, ":")
+		if len(parts) != 2 {
+			continue
+		}
+
+		switch parts[0] {
+		case "min":
+			v, err := strconv.ParseInt(parts[1], 10, 64)
+			if err != nil {
+				return validator, fmt.Errorf("%w: min must be a valid integer", ErrInvalidRule)
+			}
+
+			validator.min = v
+		case "max":
+			v, err := strconv.ParseInt(parts[1], 10, 64)
+			if err != nil {
+				return validator, fmt.Errorf("%w: max must be a valid integer", ErrInvalidRule)
+			}
+
+			validator.max = v
+		case "in":
+			numbers := strings.Split(parts[1], ",")
+			validator.in = make([]int64, 0, len(numbers))
+
+			for _, n := range numbers {
+				v, err := strconv.ParseInt(n, 10, 64)
+				if err != nil {
+					return validator, fmt.Errorf("%w: in must contain valid intergers", ErrInvalidRule)
+				}
+				validator.in = append(validator.in, v)
+			}
+		}
+	}
+
+	return validator, nil
+}
+
+func (v stringValidator) validateSlice(fieldName string, items []string) ValidationErrors {
+	if len(items) == 0 {
+		return nil
+	}
+
+	var errs ValidationErrors
+
+	for i := range items {
+		if err := v.validate(items[i]); err != nil {
+			errs = append(errs, ValidationError{
+				Err:   err,
+				Field: fmt.Sprintf("%s.%d", fieldName, i),
+			})
+		}
+	}
+
+	return errs
+}
+
+func (v intValidator) validateSlice(fieldName string, items []int) ValidationErrors {
+	if len(items) == 0 {
+		return nil
+	}
+
+	var errs ValidationErrors
+
+	for i := range items {
+		if err := v.validate(int64(items[i])); err != nil {
+			errs = append(errs, ValidationError{
+				Err:   err,
+				Field: fmt.Sprintf("%s.%d", fieldName, i),
+			})
+		}
+	}
+
+	return errs
+}
+
+func (v stringValidator) validate(item string) error {
+	if v.len != 0 && utf8.RuneCountInString(item) != v.len {
+		return fmt.Errorf("%w %d", ErrExactLen, v.len)
+	}
+
+	if v.re != nil && v.re.FindString(item) != item {
+		return fmt.Errorf("%w %s", ErrMatchRegExp, v.re.String())
+	}
+
+	if len(v.in) > 0 {
+		var found bool
+		for i := range v.in {
+			if v.in[i] == item {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("%w %q", ErrNotInList, v.in)
+		}
+	}
+
+	return nil
+}
+
+func (v intValidator) validate(item int64) error {
+	if v.min != 0 && item < v.min {
+		return fmt.Errorf("%w %d", ErrGreaterOrEqual, v.min)
+	}
+
+	if v.max != 0 && item > v.max {
+		return fmt.Errorf("%w %d", ErrLessOrEqual, v.max)
+	}
+
+	if len(v.in) > 0 {
+		var found bool
+		for i := range v.in {
+			if v.in[i] == item {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("%w %q", ErrNotInList, v.in)
+		}
+	}
+
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,7 +2,9 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -34,27 +36,185 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	InvalidRuleLen struct {
+		ID string `validate:"len:short"`
+	}
+	InvalidRuleMin struct {
+		Age int `validate:"min:youngster"`
+	}
+	InvalidRuleMax struct {
+		Age int `validate:"max:adult"`
+	}
+	InvalidRuleInteger struct {
+		Code int `validate:"in:one,two"`
+	}
+	InvalidRuleRegExp struct {
+		Email string `validate:"regexp:(@"`
+	}
 )
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name:        "01 - some string - Error",
+			in:          "not a struct",
+			expectedErr: ErrUnsupportedType,
 		},
-		// ...
-		// Place your code here.
+		{
+			name:        "02 - some number - Error",
+			in:          1234,
+			expectedErr: ErrUnsupportedType,
+		},
+		{
+			name: "03 - User valid",
+			in: User{
+				Name:   "all fields valid",
+				ID:     "testID123456789101112131415161789012",
+				Age:    45,
+				Email:  "test@mail.test",
+				Role:   "admin",
+				Phones: []string{"89611111118", "89614111741"},
+				meta:   nil,
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "04 - User invalid content",
+			in: User{
+				Name:   "all fields invalid",
+				ID:     "invalid",
+				Age:    450,
+				Email:  "testmail.test",
+				Role:   "unknown",
+				Phones: []string{"8961", "821265"},
+				meta:   nil,
+			},
+			expectedErr: ValidationErrors{
+				{Field: "ID", Err: ErrExactLen},
+				{Field: "Age", Err: ErrLessOrEqual},
+				{Field: "Email", Err: ErrMatchRegExp},
+				{Field: "Role", Err: ErrNotInList},
+				{Field: "Phones.0", Err: ErrExactLen},
+				{Field: "Phones.1", Err: ErrExactLen},
+			},
+		},
+		{
+			name: "05 - User - invalid content",
+			in: User{
+				Name:   "one phone is invalid age is less then needed",
+				ID:     "testID123456789101112131415161789012",
+				Age:    12,
+				Email:  "test@mail.test",
+				Role:   "admin",
+				Phones: []string{"89611111118", "8961741"},
+				meta:   nil,
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Age", Err: ErrGreaterOrEqual},
+				{Field: "Phones.1", Err: ErrExactLen},
+			},
+		},
+		{
+			name:        "06 - App - valid content",
+			in:          App{Version: "1.2.3"},
+			expectedErr: nil,
+		},
+		{
+			name: "07 - App - error version",
+			in:   App{Version: "1.2"},
+			expectedErr: ValidationErrors{
+				{Field: "Version", Err: ErrExactLen},
+			},
+		},
+		{
+			name: "08 - Token - valid",
+			in: Token{
+				Header:    []byte("test"),
+				Payload:   []byte("without"),
+				Signature: []byte("validation"),
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "09 - Response - valid",
+			in: Response{
+				Code: 200,
+				Body: "OK",
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "10 - Response invalid",
+			in: Response{
+				Code: 401,
+				Body: "Unauthorized",
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Code", Err: ErrNotInList},
+			},
+		},
+		{
+			name:        "11 - validate rule len",
+			in:          InvalidRuleLen{ID: "ID123456"},
+			expectedErr: ErrInvalidRule,
+		},
+		{
+			name:        "12 - validate rule min",
+			in:          InvalidRuleMin{Age: 12},
+			expectedErr: ErrInvalidRule,
+		},
+		{
+			name:        "13 - validate rule max",
+			in:          InvalidRuleMax{Age: 50},
+			expectedErr: ErrInvalidRule,
+		},
+		{
+			name:        "14 - validate rule integer",
+			in:          InvalidRuleInteger{Code: 200},
+			expectedErr: ErrInvalidRule,
+		},
+		{
+			name:        "15 - validate rule RegExp",
+			in:          InvalidRuleRegExp{Email: "test@mail.test"},
+			expectedErr: ErrInvalidRule,
+		},
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("case %s", tt.name), func(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+
+			require.Error(t, err)
+
+			var wantValidateErrs ValidationErrors
+			if errors.As(tt.expectedErr, &wantValidateErrs) {
+				var gotValidateErrs ValidationErrors
+
+				require.ErrorAs(t, err, &gotValidateErrs)
+				require.Len(t, gotValidateErrs, len(wantValidateErrs))
+
+				for j, gotE := range gotValidateErrs {
+					wantE := wantValidateErrs[j]
+
+					require.Equal(t, wantE.Field, gotE.Field)
+					require.ErrorIs(t, gotE.Err, wantE.Err)
+				}
+			} else {
+				require.ErrorIs(t, err, tt.expectedErr)
+			}
 		})
 	}
 }
